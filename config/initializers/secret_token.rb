cfab66d9b41aaba1f9bfefd8b1155af0ab8577da
pankona <yosuke.akatsuka@gmail.com>
2014-10-29 18:51:37 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RaspiRemoteBlink::Application.config.secret_key_base = 'ed8dbfedb6a036341dab48288ff7efe7032bde81b8fba5396f1abe93371e5eb648e604218da61fa2d7ddc964143dbbe48ce8f7403238add0b931e21721def5b7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RaspiRemoteBlink::Application.config.session_store :cookie_store, key: '_raspi_remote_blink_session'
